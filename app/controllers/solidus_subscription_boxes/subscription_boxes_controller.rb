05a031280d611e0840de03c797a6c24434cc0f0c
Joel Jackson <jackson.joel@gmail.com>
2017-09-09 00:38:58 -0700

    Sets the meal preference on order submit.

--- app/controllers/solidus_subscription_boxes/subscription_boxes_controller.rb
+++ app/controllers/solidus_subscription_boxes/subscription_boxes_controller.rb
@@ -1,6 +1,6 @@
 class SolidusSubscriptionBoxes::SubscriptionBoxesController < Spree::StoreController
   def get_started
     @box_variants = SolidusSubscriptionBoxes::SubscriptionPeriod.current.variants
-    @variant_id = Spree::Variant.where(sku: 'ELMLUNCHES').pluck(:id).first
+    @variant_id = Spree::Variant.first.id
   end
 end
