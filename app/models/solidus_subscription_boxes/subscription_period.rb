80fd32fcfd71bfd4abc8d6c0647adfcd4f9ff501
Joel Jackson <jackson.joel@gmail.com>
2017-09-18 23:39:42 -0700

    Allows a subscription period to be attached to a box able variant

--- app/models/solidus_subscription_boxes/subscription_period.rb
+++ app/models/solidus_subscription_boxes/subscription_period.rb
@@ -2,6 +2,7 @@ module SolidusSubscriptionBoxes
   class SubscriptionPeriod < ApplicationRecord
     has_many :subscription_periods_variants
     has_many :variants, through: :subscription_periods_variants, class_name: 'Spree::Variant'
+    belongs_to :boxable_variant, class_name: 'Spree::Variant', foreign_key: 'boxable_variant_id'
 
     def self.current
       SubscriptionPeriod.where('start_date <= ?', Date.today).order('start_date desc').last
