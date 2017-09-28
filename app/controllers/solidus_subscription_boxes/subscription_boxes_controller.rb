8c397d50a6855abd4ae6fb51e84c14bc3fb2be07
Joel Jackson <jackson.joel@gmail.com>
2017-09-27 19:27:31 -0700

    Makes the subscription box work on the variant level and add to cart.

--- app/controllers/solidus_subscription_boxes/subscription_boxes_controller.rb
+++ app/controllers/solidus_subscription_boxes/subscription_boxes_controller.rb
@@ -1,6 +1,6 @@
 class SolidusSubscriptionBoxes::SubscriptionBoxesController < Spree::StoreController
   def get_started
-  	@variant_id = Spree::Product.find_by_slug(params[:product_id]).master
-    @box_variants = SolidusSubscriptionBoxes::SubscriptionPeriod.where(boxable_variant_id: @variant_id).current.variants
+  	@variant = Spree::Product.find_by_slug(params[:product_id]).master
+    @box_variants = SolidusSubscriptionBoxes::SubscriptionPeriod.where(boxable_variant_id: @variant.id).current.variants
   end
 end
