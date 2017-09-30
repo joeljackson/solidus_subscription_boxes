62e4fc3f82b617a870fdb36883311ac7eb7789ae
Joel Jackson <jackson.joel@gmail.com>
2017-09-30 14:57:40 -0700

    Updates the actionable date when the user clicks skip

--- app/assets/javascripts/solidus_subscription_boxes/subscription_selection.js
+++ app/assets/javascripts/solidus_subscription_boxes/subscription_selection.js
@@ -1,4 +1,19 @@
 // Meal Selection Page
+function subscriptionFormatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  
+  return monthNames[monthIndex] + ' ' + day;
+}
+
+
 $('.meal-selection').click( function(event) {
   if( $('body.get_started').length > 0 ) {
     var submitButtonText = "Subscribe Now";
@@ -6,13 +21,13 @@ $('.meal-selection').click( function(event) {
     var submitButtonText = "Update My Order";
   }
 
-  if( $('.meal-selection input:checked').length > 3) {
+  if( $('.meal-selection input:checked').length > 3 ) {
     alert("Please only pick 3 meals");
     event.target.checked = false;
     event.preventDefault();
   }
 
-  if( $('.meal-selection input:checked').length == 3) {
+  if( $('.meal-selection input:checked').length == 3 ) {
     $('input[type=submit].meal-update').removeAttr('disabled');
     $('input[type=submit].meal-update').attr("value",submitButtonText);
   } else {
@@ -22,3 +37,12 @@ $('.meal-selection').click( function(event) {
 
   $('.number_more_meals').text(3 - $('.meal-selection input:checked').length);
 });
+
+$(document).ajaxSuccess(function( event, xhr, settings ) {
+  if( settings.url.match(/\/api\/v1\/subscriptions\/\d*\/skip/) ) {
+    var actionableDate = new Date(Date.parse(xhr.responseJSON.actionable_date));
+    $('.actionable-date-update').text(subscriptionFormatDate(actionableDate));
+  }
+});
+
+
