225e89df75daaa8bfa4eeb03c1269b526108db38
Joel Jackson <jackson.joel@gmail.com>
2017-09-30 14:13:59 -0700

    Makes the subscription management javascript give sensible messages.

--- app/assets/javascripts/solidus_subscription_boxes/subscription_selection.js
+++ app/assets/javascripts/solidus_subscription_boxes/subscription_selection.js
@@ -1,5 +1,11 @@
 // Meal Selection Page
 $('.meal-selection').click( function(event) {
+  if( $('body.get_started').length > 0 ) {
+    var submitButtonText = "Subscribe Now";
+  } else {
+    var submitButtonText = "Update My Order";
+  }
+
   if( $('.meal-selection input:checked').length > 3) {
     alert("Please only pick 3 meals");
     event.target.checked = false;
@@ -7,11 +13,11 @@ $('.meal-selection').click( function(event) {
   }
 
   if( $('.meal-selection input:checked').length == 3) {
-    $('input[type=submit]').removeAttr('disabled');
-    $('input[type=submit]').attr("value","Subscribe Now");
+    $('input[type=submit].meal-update').removeAttr('disabled');
+    $('input[type=submit].meal-update').attr("value",submitButtonText);
   } else {
-    $('input[type=submit]').attr("disabled","disabled");
-    $('input[type=submit]').attr("value","Choose " + (3 - $('.meal-selection input:checked').length).toString() + " More");
+    $('input[type=submit].meal-update').attr("disabled","disabled");
+    $('input[type=submit].meal-update').attr("value","Choose " + (3 - $('.meal-selection input:checked').length).toString() + " More");
   }
 
   $('.number_more_meals').text(3 - $('.meal-selection input:checked').length);
