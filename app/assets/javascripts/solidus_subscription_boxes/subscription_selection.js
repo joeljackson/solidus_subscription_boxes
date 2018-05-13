a8101ed40ed1c98ff7f16b4a03e20996676bf92b
Jackson <jjackson@THC0437.local>
2018-05-12 18:51:35 -0700

    Updates to load meal selection on turbolinks load.

--- app/assets/javascripts/solidus_subscription_boxes/subscription_selection.js
+++ app/assets/javascripts/solidus_subscription_boxes/subscription_selection.js
@@ -1,64 +1,82 @@
 // Meal Selection Page
 function subscriptionFormatDate(date) {
   var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
   ];
 
   var day = date.getDate();
   var monthIndex = date.getMonth();
-  
-  return monthNames[monthIndex] + ' ' + day;
+
+  return monthNames[monthIndex] + " " + day;
 }
 
-$('.box-controls i').click(function(event) {
-  var value = parseInt($(this).siblings('input').val());
-  if(this.classList.contains('add')) {
-    value = Math.min(value + 1, 9);
-  } else {
-    value = Math.max(value - 1, 0);
-  }
-  $(this).siblings('input').val(value);
+var setupMealSelection = function() {
+  $(".box-controls i").click(function(event) {
+    var value = parseInt(
+      $(this)
+        .siblings("input")
+        .val()
+    );
+    if (this.classList.contains("add")) {
+      value = Math.min(value + 1, 9);
+    } else {
+      value = Math.max(value - 1, 0);
+    }
+    $(this)
+      .siblings("input")
+      .val(value);
 
+    var numberOfMeals = function() {
+      var total = 0;
+      $(".box-controls input").each(function(element) {
+        total = total + parseInt(this.value);
+      });
+      return total;
+    };
 
-  var numberOfMeals = function() {
-    var total = 0;
-    $('.box-controls input').each( function(element) {
-      total = total + parseInt(this.value);
-    });
-    return total;
-  }
+    if ($("body.get_started").length > 0) {
+      var submitButtonText = "Buy Now";
+    } else {
+      var submitButtonText = "Update My Order";
+    }
 
-  if( $('body.get_started').length > 0 ) {
-    var submitButtonText = "Buy Now";
-  } else {
-    var submitButtonText = "Update My Order";
-  }
+    if (numberOfMeals() >= 1) {
+      $("input[type=submit].meal-update").removeAttr("disabled");
+      $("input[type=submit].meal-update").attr("value", submitButtonText);
+    } else {
+      $("input[type=submit].meal-update").attr("disabled", "disabled");
+      $("input[type=submit].meal-update").attr(
+        "value",
+        "Choose " + Math.max(3 - numberOfMeals(), 0).toString() + " More"
+      );
+    }
 
-  if( numberOfMeals() >= 1 ) {
-    $('input[type=submit].meal-update').removeAttr('disabled');
-    $('input[type=submit].meal-update').attr("value",submitButtonText);
-  } else {
-    $('input[type=submit].meal-update').attr("disabled","disabled");
-    $('input[type=submit].meal-update').attr("value","Choose " + (Math.max(3 - numberOfMeals(), 0)).toString() + " More");
-  }
+    $(".number_more_meals").text(1 - numberOfMeals());
+  });
+};
 
-  $('.number_more_meals').text(1 - numberOfMeals());
-});
+$(document).on("turbolinks:load", setupMealSelection);
 
-$(document).ajaxSuccess(function( event, xhr, settings ) {
-  if( settings.url.match(/\/api\/v1\/subscriptions\/\d*\/skip/) ) {
+$(document).ajaxSuccess(function(event, xhr, settings) {
+  if (settings.url.match(/\/api\/v1\/subscriptions\/\d*\/skip/)) {
     var actionableDate = new Date(Date.parse(xhr.responseJSON.actionable_date));
-    $('.actionable-date-update').text(subscriptionFormatDate(actionableDate));
+    $(".actionable-date-update").text(subscriptionFormatDate(actionableDate));
   }
-  if( settings.url.match(/\/api\/v1\/subscriptions\/\d*\/cancel/) ) {
-    $('.manage-subscription').remove();
+  if (settings.url.match(/\/api\/v1\/subscriptions\/\d*\/cancel/)) {
+    $(".manage-subscription").remove();
   }
-  if( settings.url.match(/\/api\/v1\/subscriptions\/\d*$/)) {
-    alert("You've updated your subscription.")
+  if (settings.url.match(/\/api\/v1\/subscriptions\/\d*$/)) {
+    alert("You've updated your subscription.");
   }
 });
-
-
